508f563f4fc9811afe0857d56619418cda9b64f6
zeeone <82263175+zeeoneofc@users.noreply.github.com>
2021-06-06 03:19:53 +0800

    Update termux_install.sh

--- termux_install.sh
+++ termux_install.sh
@@ -34,8 +34,8 @@ clear
 pkg update && pkg upgrade -y
 pkg install clang curl git libcrypt libffi libiconv libjpeg* libjpeg-turbo libwebp libxml2 libxslt make ndk-sysroot openssl postgresql python readline wget zlib -y
 
-git clone https://github.com/apisuserbot/King-Userbot.git
-cd Lord-Userbot
+git clone https://github.com/zeeoneofc/Alpha-userbot.git
+cd Alpha-userbot
 
 pip install --upgrade pip setuptools
 pip install -r requirements.txt
@@ -50,7 +50,7 @@ createuser botuser
 
 cd ..
 echo "pg_ctl -D $PREFIX/var/lib/postgresql start" > startbot.sh
-echo "cd Lord-Userbot" >> startbot.sh
+echo "cd Alpha-userbot" >> startbot.sh
 echo "python3 -m userbot" >> startbot.sh
 chmod 755 startbot.sh
 
