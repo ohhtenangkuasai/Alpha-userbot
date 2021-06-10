6d476dc84f30466cd9e4ddbd3508223eab4245a2
zeeone <82263175+zeeoneofc@users.noreply.github.com>
2021-06-10 09:38:26 +0800

    @zeeoneofc

--- userbot/modules/kinghelper.py
+++ userbot/modules/kinghelper.py
@@ -11,33 +11,33 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 @register(outgoing=True, pattern="^.khelp$")
 async def usit(e):
     await e.edit(
-        f"      ╔════════════╗\n     ⚡️𝘽𝘼𝙉𝙏𝙐𝘼𝙉⚡️     \n╚════════════╝ \n"
+        f"      ╭✠╼━━━━━━❖━━━━━━━✠╮\n     **BENTUAN**     \n╰✠╼━━━━━━❖━━━━━━━✠╯ \n"
         f"**Hai {DEFAULTUSER} Kalau Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
-        "═⎆ Pemilik : [Mr zeeone](t.me/zeeoneee) \n"
-        "═⎆ Repo    : [Repo](https://github.com/zeeoneofc/Alpha-userbot) \n"
-        "═⎆ Instragam : [Instagram](Instagram.com/zeeoneofc) \n"
-        "═⎆ Grup Support : [Alpha Userbot Support](https://t.me/alphabot_support)")
+        "◍ ᴏᴡɴᴇʀ  : [ZEEONE](t.me/zeeoneee) \n"
+        "◍ ʀᴇᴘᴏ    : [klik](https://github.com/zeeoneofc/Alpha-userbot) \n"
+        "◍ ɪɴꜱᴛᴀɢʀᴀᴍ : [klik](Instagram.com/zeeoneofc) \n"
+        "◍ ɢʀᴏᴜᴘ ꜱᴜᴘᴘᴏʀᴛ : [Klik](https://t.me/alphabot_support)")
 
 
 @register(outgoing=True, pattern="^.vars$")
 async def var(m):
     await m.edit(
-        f"      ╔════════════╗\n  ⚡️𝘿𝘼𝙁𝙏𝘼𝙍 𝙑𝘼𝙍𝙎⚡️     \n╚════════════╝ \n"
-        f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "═⎆ Daftar Vars : [DAFTAR VARS](https://raw.githubusercontent.com/zeeoneofc/Alpha-userbot/Alpha-userbot/varshelper.txt)")
+        f"      ╭✠╼━━━━━━❖━━━━━━━✠╮\n  **DAFTAR VARS**     \n╰✠╼━━━━━━❖━━━━━━━✠╯ \n"
+        f"**Daftar Vars Dari {DEFAULTUSER}:**\n"
+        "◍ ᴅᴀꜰᴛᴀʀ ᴠᴀʀꜱ = [Klik](https://raw.githubusercontent.com/zeeoneofc/Alpha-userbot/Alpha-userbot/varshelper.txt)")
 
 
 CMD_HELP.update(
     {
         "helper": "**✘ Plugin : **`helper`\
         \n\n  •  **Perintah :** `.khelp`\
-        \n  •  **Function : **Bantuan Untuk 💠 Alpha-userbot 💠.\
+        \n  •  **Function : **Bantuan Untuk **✵ ALPHABOT ✵**.\
         \n\n  •  **Perintah :** `.vars`\
         \n  •  **Function : **Melihat Daftar Vars.\
         \n\n  •  **Perintah :** `.repo`\
-        \n  •  **Function : **Melihat Repo 💠 Alpha-userbot 💠.\
+        \n  •  **Function : **Melihat Repo **✵ ALPHABOT ✵**.\
         \n\n  •  **Perintah :** `.string`\
-        \n  •  **Function : **Link untuk mengambil String 💠 Alpha-userbot 💠.\
+        \n  •  **Function : **Link untuk mengambil String **✵ ALPHABOT ✵**.\
     "
     }
 )
